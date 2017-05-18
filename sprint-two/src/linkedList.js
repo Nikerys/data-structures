311195798cfe315c773b7bb16d8ff577f0e0bb49
Hack Reactor Student <student@hackreactor.com>
2017-05-17 22:14:49 -0700

    started linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,24 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let node = Node(value);
+    // if (this.tail.value !== null) {
+      
+    // }
+    this.tail = node;
   };
 
   list.removeHead = function() {
+    // this.head = null;
   };
 
   list.contains = function(target) {
+  //   for (var key in list) {
+  //     if (key === target) {
+  //       return target;
+  //     }
+  //   }
+  // };
   };
 
   return list;
