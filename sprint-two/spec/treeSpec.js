96317187281b3bb2355a2f3ec93a22b54ee7036d
Hack Reactor Student <student@hackreactor.com>
2017-05-19 20:02:13 -0700

    Finished tree

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -28,8 +28,13 @@ describe('tree', function() {
 
   it('should be able to add children to a tree\'s child', function() {
     tree.addChild(5);
-    tree.children[0].addChild(6);
-    expect(tree.children[0].children[0].value).to.equal(6);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    expect(tree.children[0].value).to.equal(5);
+    expect(tree.children[1].value).to.equal(6);
+    expect(tree.children[0].children[0].value).to.equal(7);
+    
   });
 
   it('should correctly detect nested children', function() {
@@ -37,8 +42,11 @@ describe('tree', function() {
     tree.addChild(6);
     tree.children[0].addChild(7);
     tree.children[1].addChild(8);
+    tree.children[0].children[0].addChild(9);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+    expect(tree.contains(9)).to.equal(true);
+
   });
 
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,31 +4,43 @@ var Tree = function(value) {
 
   // your code here
   newTree.children = [];
+  
   _.extend(newTree, treeMethods);
   return newTree;
 };
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.value = value;
-  this.children.push(this);
+  var childTree = Tree(value);
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
-//go through the Tree to see if it's value equals target
-//if not go into the tree's children, check length, value
-//
+  //check the length of the first children for for loop
+  //if target === value, return true
+  //else, if it has children, set a variable for its children
+  //when it no longer has children, loop stops, and then 
+    //recurse second children of the tree
+
+  if (this.value === target) {
+    return true;
+  }
+    
   for (var i = 0; i < this.children.length; i++) {
-    if (this.children[i].value === target) {
+    if (this.children[i].contains(target)) {
       return true;
-    } else {
-      this.children[i].value = this;
     }
   }
   return false;
+
 };
 
 
+    // console.log('this is value', this.children[i].value);
+    // console.log('this is target', target);    
+    // console.log('if we found target', this.children[i].value === target);     
+    
 
 /*
  * Complexity: What is the time complexity of the above functions?
