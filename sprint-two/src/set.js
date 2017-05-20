5b7f68d1140a7dd96467783c0c195a4b02a8207a
Hack Reactor Student <student@hackreactor.com>
2017-05-19 20:45:48 -0700

    finish set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //An .add() method, takes any string and adds it to the set
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  /* .contains() method, takes any string and returns 
+  a boolean reflecting whether it can be found in the set */
+  var result = false;
+  _.forEach(this.storage, function(name) {
+    console.log(name);
+    if (name === item) {
+      result = true;  
+    }
+  });
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  //A .remove() method, takes any string and removes it from the set, if present
+  this.storage = _.filter(this.storage, function(name) {
+    return name !== item;
+  });
 };
 
 /*
